0891c6ef17542b3f639368ba70dd37323fd91b84
Draphikas <thomas.bocquez@epitech.eu>
2023-11-03 16:15:31 +0100

    feat(validation): update validation for DTOs with relation

--- src/modules/country/dto/update-country.dto.ts
+++ src/modules/country/dto/update-country.dto.ts
@@ -1,4 +1,5 @@
-import { PartialType } from '@nestjs/swagger';
+import { IntersectionType } from '@nestjs/swagger';
 import { CreateCountryDto } from './create-country.dto';
+import { EntityId } from '../../../validations/common';
 
-export class UpdateCountryDto extends PartialType(CreateCountryDto) {}
+export class UpdateCountryDto extends IntersectionType(CreateCountryDto, EntityId) {}
--- src/modules/season/dto/update-season.dto.ts
+++ src/modules/season/dto/update-season.dto.ts
@@ -1,3 +1,5 @@
+import { IntersectionType } from '@nestjs/swagger';
 import { CreateSeasonDto } from './create-season.dto';
+import { EntityId } from '../../../validations/common';
 
-export class UpdateSeasonDto extends CreateSeasonDto {}
+export class UpdateSeasonDto extends IntersectionType(CreateSeasonDto, EntityId) {}
