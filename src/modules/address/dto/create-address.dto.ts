dd58ad1bcd951a8eab5b73cc9cc140a36700a170
cchampou <clement@champouillon.com>
2023-11-03 19:41:20 +0100

    fix: create DTO

--- src/modules/address/dto/create-address.dto.ts
+++ src/modules/address/dto/create-address.dto.ts
@@ -1,1 +1,18 @@
-export class CreateAddressDto {}
+import { IsNumber, IsString } from 'class-validator';
+
+export class CreateAddressDto {
+  @IsString()
+  street: string;
+
+  @IsString()
+  complement: string;
+
+  @IsString()
+  zipCode: string;
+
+  @IsNumber()
+  latitude: number;
+
+  @IsNumber()
+  longitude: number;
+}
