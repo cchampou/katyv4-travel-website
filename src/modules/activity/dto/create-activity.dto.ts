1d008e8b917a894aeec9923ab28cae287b7a54d0
Thomas BOCQUEZ <thomas.bocquez@epitech.eu>
2023-11-03 19:35:53 +0100

    fix(activity): remove address_id not compliant in activity (#29)

--- src/modules/activity/dto/create-activity.dto.ts
+++ src/modules/activity/dto/create-activity.dto.ts
@@ -1,10 +1,11 @@
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateActivityDto {
-  @IsString()
   @IsNotEmpty()
+  @IsString()
   name: string;
 
+  @IsNotEmpty()
   @IsString()
   description: string;
 }
