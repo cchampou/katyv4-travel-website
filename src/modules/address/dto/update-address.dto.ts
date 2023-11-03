0a595d52d8c215abf385fb314abd4887281e85df
cchampou <clement@champouillon.com>
2023-11-03 19:38:28 +0100

    fix: add entityId in update DTO

--- src/modules/address/dto/update-address.dto.ts
+++ src/modules/address/dto/update-address.dto.ts
@@ -1,3 +1,5 @@
 import { CreateAddressDto } from './create-address.dto';
+import { IntersectionType } from '@nestjs/swagger';
+import { EntityId } from '../../../validations/common';
 
-export class UpdateAddressDto extends CreateAddressDto {}
+export class UpdateAddressDto extends IntersectionType(CreateAddressDto, EntityId) {}
