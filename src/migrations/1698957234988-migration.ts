b1d88096f9d847b845ef32a29eafa2c60ad58c35
cchampou <clement@champouillon.com>
2023-11-03 19:36:36 +0100

    fix: rename columns in migration

--- src/migrations/1698957234988-migration.ts
+++ src/migrations/1698957234988-migration.ts
@@ -5,7 +5,7 @@ export class Migration1698957234988 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `CREATE TABLE "address" ("id" SERIAL NOT NULL, "addressLine1" character varying NOT NULL, "addressLine2" character varying NOT NULL, "zipCode" character varying NOT NULL, "latitude" double precision NOT NULL, "longitude" double precision NOT NULL, CONSTRAINT "PK_d92de1f82754668b5f5f5dd4fd5" PRIMARY KEY ("id"))`,
+      `CREATE TABLE "address" ("id" SERIAL NOT NULL, "street" character varying NOT NULL, "complement" character varying NOT NULL, "zipCode" character varying NOT NULL, "latitude" double precision NOT NULL, "longitude" double precision NOT NULL, CONSTRAINT "PK_d92de1f82754668b5f5f5dd4fd5" PRIMARY KEY ("id"))`,
     );
   }
 
