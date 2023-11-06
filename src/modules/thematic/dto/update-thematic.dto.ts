81420b3c3251d794cdbef782dabee7ba1c46ef48
Thomas Bocquez <tbocquez@steamulo.com>
2023-11-06 21:10:58 +0100

    feat(thematic): add entityId on UpdateThematicDto

--- src/modules/thematic/dto/update-thematic.dto.ts
+++ src/modules/thematic/dto/update-thematic.dto.ts
@@ -1,3 +1,5 @@
+import { IntersectionType } from '@nestjs/swagger';
 import { CreateThematicDto } from './create-thematic.dto';
+import { EntityId } from '../../../validations/common';
 
-export class UpdateThematicDto extends CreateThematicDto {}
+export class UpdateThematicDto extends IntersectionType(CreateThematicDto, EntityId) {}
